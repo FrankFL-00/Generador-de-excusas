b65dd975aa413241ec3d3cfa9ea2af5b71d11b07
FrankFL-00 <161798493+FrankFL-00@users.noreply.github.com>
2024-03-12 06:20:02 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function Palabrarandom(array) {
+    let PosicionRandom = Math.floor(Math.random() * array.length);
+    return array[PosicionRandom];
+  }
+
+  let PalabraWho = Palabrarandom(who);
+  let PalabraAction = Palabrarandom(action);
+  let PalabraWhat = Palabrarandom(what);
+  let PalabraWhen = Palabrarandom(when);
+
+  let sentence =
+    PalabraWho + " " + PalabraAction + " " + PalabraWhat + " " + PalabraWhen;
+
+  let Nuevaexcusa = document.getElementById("excuse");
+
+  Nuevaexcusa.innerHTML = sentence;
 };
